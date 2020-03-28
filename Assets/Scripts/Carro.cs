5476596b33e7a448149eb59898a16ac2b5547047
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-03-28 11:58:18 -0300

    Criado a base do jogo

---
+++ Assets/Scripts/Carro.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Carro : MonoBehaviour
+{
+    public float velocidadeMovimento = 30f;
+    public float velocidadeRotacao = 150f;
+
+    void Update()
+    {
+        var praFrente = Input.GetAxis("Vertical") * velocidadeMovimento * Time.deltaTime;
+        transform.Translate(0, 0, praFrente);
+
+        var praDireita = Input.GetAxis("Horizontal") * velocidadeRotacao * Time.deltaTime;
+        transform.Rotate(0, praDireita, 0);
+    }
+}
